e8dee682e5bd403a181204730258e554710cb336
Viktoriia <viktoriiafesyk@gmail.com>
2023-05-29 23:18:40 +0300

    configure absolute imports and create folder Container

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './style.css'
-import Title from './Title'
-import { Content } from './Content'
-
-const App = () => {
-    return (
-        <>
-            <Title title="App" />
-            <Title title="React" num={1} />
-            <Content
-                text1={
-                    'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui magnam numquam soluta repudiandae officia quaerat obcaecati maxime tempora enim culpa sed, aut eius dolores quis exercitationem animi quam eos optio?'
-                }
-                text2={
-                    'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui magnam numquam soluta repudiandae officia quaerat obcaecati maxime tempora enim culpa sed, aut eius dolores quis exercitationem animi quam eos optio?'
-                }
-            />
-        </>
-    )
-}
+import App from './Container/App/App'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
