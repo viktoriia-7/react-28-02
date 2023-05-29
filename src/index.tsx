663b2e1a50b24ad0e9f778b8b5adafb7e0718ba8
Viktoriia <viktoriiafesyk@gmail.com>
2023-05-29 18:57:55 +0300

    described props and types through interface

--- src/index.tsx
+++ src/index.tsx
@@ -2,39 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './style.css'
 
-//React element
-// const title = React.createElement('h1', { id: 'title' }, 'Hello React')
+interface TitleProps {
+    title: string
+}
 
-//JSX
-
-// const app = (
-//     <div>
-//         <h1 id="text">Hello App</h1>
-//         <p>
-//             Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
-//             est ipsum quae amet! At omnis ea similique enim impedit sunt.
-//         </p>
-//     </div>
-// )
-
-//React component (виклик-ся як тег <App />)
 const App = () => {
     return (
         <>
-            <Title />
+            <Title title="App" />
+            <Title title="React" />
             <Content />
         </>
     )
 }
 
-const Title = () => {
-    return <h1>Hello App compoment</h1>
+const Title = (props: TitleProps) => {
+    return <h1>Hello {props.title} compoment</h1>
 }
-//React.Fragment or <> </>, це прибирає зайвий рівень вложеності <div>
+
 const Content = () => {
     return (
         <React.Fragment>
-            <p className = "blue">
+            <p className="blue">
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui
                 magnam numquam soluta repudiandae officia quaerat obcaecati
                 maxime tempora enim culpa sed, aut eius dolores quis
