e9c6e5724dae2d6632ac10dbe174501d717875f9
Viktoriia <viktoriiafesyk@gmail.com>
2023-05-30 18:28:20 +0300

    added Main and Home components

--- src/Container/App/App.tsx
+++ src/Container/App/App.tsx
@@ -1,12 +1,14 @@
 import Header from 'Container/Header/Header'
 import CssBaseline from '@mui/material/CssBaseline'
 import { StyledEngineProvider } from '@mui/material/styles'
+import Main from 'Container/Main/Main'
 
 const App = () => {
     return (
         <StyledEngineProvider injectFirst>
             <CssBaseline />
             <Header />
+            <Main />
         </StyledEngineProvider>
     )
 }
