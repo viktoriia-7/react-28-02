4f5c5bb75fb2e84eb7f8ffcfae4e80f00a954bdb
Viktoriia <viktoriiafesyk@gmail.com>
2023-06-07 21:39:20 +0300

    wrote a classic version of the function of working with state in class components

--- src/components/Products/ProductListItem.tsx
+++ src/components/Products/ProductListItem.tsx
@@ -18,31 +18,48 @@ type Props = {
     image: string
 }
 
-class ProductListItem extends React.Component<Props> {
+type State = {
+    count: number
+}
+
+class ProductListItem extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props)
+        this.state = {
+            count: 1,
+        }
+        this.onIncrementClick = this.onIncrementClick.bind(this)
+    }
+
+    onIncrementClick() {
+        this.setState((prevState) => ({
+            count: prevState.count + 1,
+        }))
+    }
     render() {
+        const { title, description, type, capacity, price, image } = this.props
         return (
             <Card className="product" variant="outlined">
                 <CardContent>
                     <div className="product-img">
-                        <img src={this.props.image} alt="" />
+                        <img src={image} alt="" />
                     </div>
-                    <h4 className="product-title">{this.props.title}</h4>
-                    <div className="product-description">
-                        {this.props.description}
-                    </div>
+                    <h4 className="product-title">{title}</h4>
+                    <div className="product-description">{description}</div>
+                    <div className="product-features">Type: {type}</div>
                     <div className="product-features">
-                        Type: {this.props.type}
+                        Capacity: {capacity}Gb
                     </div>
-                    <div className="product-features">
-                        Capacity: {this.props.capacity}Gb
-                    </div>
-                    <div className="product-price">
-                        Price: $ {this.props.price}
-                    </div>
+                    <div className="product-price">Price: $ {price}</div>
                     <div className="product-quantity">
                         <Button variant="outlined">-</Button>
-                        <TextField value="1" size="small" />
-                        <Button variant="outlined">+</Button>
+                        <TextField value={this.state.count} size="small" />
+                        <Button
+                            variant="outlined"
+                            onClick={this.onIncrementClick}
+                        >
+                            +
+                        </Button>
                     </div>
                     <CardActions className="btn-wrap">
                         <Button variant="outlined">Add to cart</Button>
@@ -53,14 +70,4 @@ class ProductListItem extends React.Component<Props> {
     }
 }
 
-// const ProductListItem = ({
-//     title,
-//     description,
-//     type,
-//     capacity,
-//     price,
-//     image,
-// }: Props) => {
-
-// }
 export default ProductListItem
