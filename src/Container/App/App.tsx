a94dd205539b27b2d491b10b281aba20b4c3756f
Viktoriia <viktoriiafesyk@gmail.com>
2023-06-13 17:05:13 +0300

    added productsInCart

--- src/Container/App/App.tsx
+++ src/Container/App/App.tsx
@@ -4,26 +4,21 @@ import { StyledEngineProvider } from '@mui/material/styles'
 import Main from 'Container/Main/Main'
 import { useState } from 'react'
 
-type CartData = {
-    totalCount: number
-    totalPrice: number
+type ProductsInCart = {
+    [id: number]: number
 }
+
 const App = () => {
-    const [cartData, setCartData] = useState<CartData>({
-        totalCount: 0,
-        totalPrice: 0,
+    const [productsInCart, setProductsInCar] = useState<ProductsInCart>({
+        1: 5,
+        2: 5,
     })
 
-    const addProductToCart = (count: number, price: number) => {
-        setCartData((prevState) => ({
-            totalCount: prevState.totalCount + count,
-            totalPrice: prevState.totalPrice + count * price,
-        }))
-    }
+    const addProductToCart = (count: number, price: number) => {}
     return (
         <StyledEngineProvider injectFirst>
             <CssBaseline />
-            <Header cartData={cartData} />
+            <Header productsInCart={productsInCart} />
             <Main addProductToCart={addProductToCart} />
         </StyledEngineProvider>
     )
--- src/Container/Header/Header.tsx
+++ src/Container/Header/Header.tsx
@@ -9,12 +9,11 @@ import Menu from 'components/Menu/Menu'
 import CartHeader from 'components/CartHeader/CartHeader'
 
 type Props = {
-    cartData: {
-        totalCount: number
-        totalPrice: number
+    productsInCart: {
+        [id: number]: number
     }
 }
-const Header = ({ cartData }: Props) => {
+const Header = ({ productsInCart }: Props) => {
     return (
         <AppBar position="static" className="app-bar">
             <Container>
@@ -30,7 +29,7 @@ const Header = ({ cartData }: Props) => {
                     </IconButton>
                     <Logo />
                     <Menu />
-                    <CartHeader cartData={cartData} />
+                    <CartHeader productsInCart={productsInCart} />
                 </Toolbar>
             </Container>
         </AppBar>
