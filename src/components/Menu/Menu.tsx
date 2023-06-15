ea4c4c07540fa70de22332c60f253d53b5ed9122
Viktoriia <viktoriiafesyk@gmail.com>
2023-06-15 16:17:02 +0300

    added MenuItem

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,20 +1,15 @@
-import Button from '@mui/material/Button'
-import { NavLink } from 'react-router-dom'
+import MenuItem from './MenuItem'
 
 type Props = {}
 
 const Menu = (props: Props) => {
     return (
         <>
-            <Button color="inherit">
-                <NavLink to="/">Home</NavLink>
-            </Button>
-            <Button color="inherit">About</Button>
-            <Button color="inherit">Payment</Button>
-            <Button color="inherit">Shipping</Button>
-            <Button color="inherit">
-                <NavLink to="/cart">Cart</NavLink>
-            </Button>
+            <MenuItem to="/">Home</MenuItem>
+            <MenuItem to="/about">About</MenuItem>
+            <MenuItem to="/payment">Payment</MenuItem>
+            <MenuItem to="/shipping">Shipping</MenuItem>
+            <MenuItem to="/cart">Cart</MenuItem>
         </>
     )
 }
