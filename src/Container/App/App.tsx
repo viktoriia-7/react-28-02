14ff8fe9d9d026820adb6f1501b2bc7654488cb0
Viktoriia <viktoriiafesyk@gmail.com>
2023-06-15 15:48:18 +0300

    added ReactRouter

--- src/Container/App/App.tsx
+++ src/Container/App/App.tsx
@@ -1,8 +1,11 @@
 import Header from 'Container/Header/Header'
 import CssBaseline from '@mui/material/CssBaseline'
 import { StyledEngineProvider } from '@mui/material/styles'
-import Main from 'Container/Main/Main'
 import { useState } from 'react'
+import { Container } from '@mui/material'
+import { Routes, Route } from 'react-router-dom'
+import Home from 'pages/Home/Home'
+import CartPage from 'pages/Cart/CartPage'
 
 type ProductsInCart = {
     [id: number]: number
@@ -24,7 +27,19 @@ const App = () => {
         <StyledEngineProvider injectFirst>
             <CssBaseline />
             <Header productsInCart={productsInCart} />
-            <Main addProductToCart={addProductToCart} />
+            <Container
+                sx={{
+                    padding: '40px 0',
+                }}
+            >
+                <Routes>
+                    <Route
+                        path="/"
+                        element={<Home addProductToCart={addProductToCart} />}
+                    />
+                    <Route path="/cart" element={<CartPage />} />
+                </Routes>
+            </Container>
         </StyledEngineProvider>
     )
 }
--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,15 +1,20 @@
 import Button from '@mui/material/Button'
+import { NavLink } from 'react-router-dom'
 
 type Props = {}
 
 const Menu = (props: Props) => {
     return (
         <>
-            <Button color="inherit">Home</Button>
+            <Button color="inherit">
+                <NavLink to="/">Home</NavLink>
+            </Button>
             <Button color="inherit">About</Button>
             <Button color="inherit">Payment</Button>
             <Button color="inherit">Shipping</Button>
-            <Button color="inherit">Cart</Button>
+            <Button color="inherit">
+                <NavLink to="/cart">Cart</NavLink>
+            </Button>
         </>
     )
 }
