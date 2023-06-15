a86a7c71004e56a7194b151261b05d0931ccc760
Viktoriia <viktoriiafesyk@gmail.com>
2023-06-15 12:41:04 +0300

    added total in CartHeader

--- src/components/CartHeader/CartHeader.tsx
+++ src/components/CartHeader/CartHeader.tsx
@@ -14,12 +14,24 @@ const CartHeader = ({
 }: Props) => {
     return (
         <div>
-            {Object.keys(productsInCart).map((productId) => (
-                <div key={productId}>
-                    {productsObject[parseInt(productId)].title}:{''}
-                    {productsInCart[parseInt(productId)]}
-                </div>
-            ))}
+            <div>
+                {Object.keys(productsInCart).map((productId) => (
+                    <div key={productId}>
+                        {productsObject[parseInt(productId)].title}:{''}
+                        {productsInCart[parseInt(productId)]}
+                    </div>
+                ))}
+            </div>
+            <div>
+                Total: ${' '}
+                {Object.keys(productsInCart).reduce(
+                    (total, productId) =>
+                        total +
+                        productsInCart[parseInt(productId)] *
+                            productsObject[parseInt(productId)].price,
+                    0
+                )}
+            </div>
         </div>
     )
 }
